c6a320db540450e508045b7ff26195f16b9a31a6
Farhad <farhadzohoorz@gmail.com>
2021-12-29 09:46:25 +0330

    slight change to part2 solution of question 1

--- 2021/1/part2.cpp
+++ 2021/1/part2.cpp
@@ -5,64 +5,92 @@
 
 using namespace std;
 
+bool is_number(const string& s)
+{
+    string::const_iterator it = s.begin();
+    while (it != s.end() && isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
+
 int main()
 {
-    unordered_map<int, vector<string>> mapOfNumberList;
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // unordered_map<int, vector<string>> mapOfNumberList;
+    // unordered_map<string, int> charToNumber;
+    // string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    string currentLine;
+    // string currentLine;
+
+    // while (true)
+    // {
+    //     vector<string> inputChars;
+
+    //     getline(cin, currentLine);
+    //     if (currentLine.empty())
+    //         break;
+
+    //     stringstream ss(currentLine);
+    //     string word;
+
+    //     while (ss >> word)
+    //     {
+    //         inputChars.push_back(word);
+    //     }
+
+    //     int height = stoi(inputChars[0]);
+
+    //     vector<string> characters;
+
+    //     for (auto ch : inputChars)
+    //     {
+    //         if (!is_number(ch))
+    //         {
+    //             characters.push_back(ch);
+    //         }
+    //     }
+
+    //     for (auto character : characters)
+    //     {
+    //         charToNumber[character] += height;
+    //     }
+    // }
+
+    // int increases = 0;
+    // int previousNumber = INT_MIN;
+
+    // for (auto character : alphabet)
+    // {
+    //     string characterString(1, character);
+    //     int sumOfHeights = charToNumber[characterString];
+
+    //     if (sumOfHeights > previousNumber)
+    //     {
+    //         increases++;
+    //         cout << characterString << ": " << sumOfHeights << endl;
+    //         previousNumber = sumOfHeights;
+    //     }
+    // }
+
+    // cout << increases - 1;
+
+
+
+
+    int previousNumber = INT_MIN;
+    int currentNumber = 0;
+    int increases = 0;
+    string currentNumberString;
 
     while (true)
     {
-        vector<string> chars;
-
-        getline(cin, currentLine);
-        if (currentLine.empty())
-        {
+        getline(cin, currentNumberString);
+        if (currentNumberString.empty())
             break;
-        }
 
-        stringstream ss(currentLine);
-
-        string word;
-        string heightString;
-
-        ss >> heightString;
-
-        while (getline(ss, word, ' '))
-        {
-            chars.push_back(word);
-        }
-
-        int height = stoi(heightString);
-
-        mapOfNumberList[height] = chars;
-    }
-
-    unordered_map<string, int> charToNumber;
-
-    for (auto heightList : mapOfNumberList)
-    {
-        for (auto character : heightList.second)
-        {
-            charToNumber[character] += heightList.first;
-        }
-    }
-
-    int increases = 0;
-    int previousNumber = INT_MIN;
-
-    for (auto character : alphabet)
-    {
-        string characterString(1, character);
-        int sumOfHeights = charToNumber[characterString];
-
-        if (sumOfHeights > previousNumber)
-        {
+        currentNumber = stoi(currentNumberString);
+        if (currentNumber > previousNumber)
             increases++;
-            cout << characterString << ": " << sumOfHeights << endl;
-            previousNumber = sumOfHeights;
-        }
+
+        previousNumber = currentNumber;
     }
 
     cout << increases - 1;
